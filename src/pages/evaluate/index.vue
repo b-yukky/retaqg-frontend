0a932076a1c346a5ba7b3a8ac2506ed34f1873f0
Bryan Djafer <62521415+b-yukky@users.noreply.github.com>
2022-12-15 20:30:11 +0900

    generate questions done

--- src/pages/evaluate/index.vue
+++ src/pages/evaluate/index.vue
@@ -3,6 +3,32 @@
 </script>
 
 <template>
-  <div>
+  <div class="d-flex justify-md-center">
+    <VCard
+      width="60%"
+      title="Evaluate questions"
+    >
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+      <VBtn
+        outlined
+        tile
+        x-large
+        >
+      Start
+      <v-icon x-large>mdi-play</v-icon>
+      </VBtn>
+      </v-card-actions>
+
+
+    </VCard>
   </div>
 </template>
+
+
+
+<route lang="yaml">
+  meta:
+    layout: evaluation
+  </route>
